4294516b6e4b260ec036483f1e3853c64723634f
divassahu <divassahu@gmail.com>
2022-11-30 17:35:36 +0530

    mini project uploaded

---
+++ DailyNeeds/src/test/java/com/example/dailyNeeds/DailyNeedsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dailyNeeds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyNeedsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
